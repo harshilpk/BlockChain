d79878c3dbe7798793f9d3b379a232037add3502
Harshil <harshilpk@gmail.com>
2018-11-11 14:03:24 -0700

    Added formatting. used reduce and fixed bugs in get_balance

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,4 @@
+import functools
 # Initializing our blockchain list
 MINING_REWARD = 10
 
@@ -52,17 +53,19 @@ def get_balance(participant):
     open_tx_sender = [tx['amount']
                       for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,tx_sender, 0)
+    # amount_sent = 0
+    # for tx in tx_sender:
+    #     if len(tx) > 0:
+    #         amount_sent += tx[0]
 
     tx_recipient = [[tx['amount'] for tx in block['transactions']
                      if tx['recipient'] == participant] for block in blockchain]
-    amount_recieved = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_recieved += tx[0]
+    amount_recieved = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,tx_recipient, 0)
+    # amount_recieved = 0
+    # for tx in tx_recipient:
+    #     if len(tx) > 0:
+    #         amount_recieved += tx[0]
     return amount_recieved - amount_sent
 
 
@@ -200,7 +203,7 @@ while waiting_for_input:
             print('Added transaction!')
         else:
             print('Transaction failed!')
-        # print(open_transactions)
+        print(open_transactions)
         # print(blockchain)
     elif user_choice == '2':
         if mine_block():
@@ -228,7 +231,7 @@ while waiting_for_input:
         print_blockchain_elements()
         print('Invalid blockchain!')
         break
-    print(get_balance('Harshil'))
+    print('Balance of {}: {:6.2f}'.format('Harshil',get_balance('Harshil')))
 
 else:
     print('User left!')
